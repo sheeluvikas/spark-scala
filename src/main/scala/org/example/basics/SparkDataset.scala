55a5cd96bd56d3c2263b8977dd9cf7d6f7bfcaa8
vikaskatiyar@live.com <vikaskatiyar@live.com>
2020-05-09 19:46:33 +0530

    Dataset, and json to parquet conversion

--- src/main/scala/org/example/basics/SparkDataset.scala
+++ src/main/scala/org/example/basics/SparkDataset.scala
@@ -6,19 +6,28 @@ import com.example.spark.utils.SparkUtils
 object SparkDataset {
 
   def main(args: Array[String]): Unit = {
+    /** Get the spark session */
     val sparkSession = SparkUtils.buildSparkContext("Spark Dataset App")
+
+    /** get the dataframe from the json object. */
     val df = sparkSession.read.json("src/main/resources/Student.json")
+
+    /** write the above dataframe into the parquet file */
     df.write.mode("overwrite").parquet("src/main/resources/parquet")
 
 
+    /** Now read the above parquet file in a dataframe */
     val studentDF = sparkSession.read.parquet("src/main/resources/parquet/")
 
+    println("****** Showing the student Dataframe from Parquet file :")
     studentDF.show()
 
     import sparkSession.implicits._
 
-    val student = studentDF.as[Student]
+    /** Convert the above dataframe in to a dataset of Student type. */
+    val student = studentDF.as[Student] /** The Student should be case class */
 
+    println("****** Printing the Student dataset :")
     student.filter(x => {
       x.age < 30
     }).show()
